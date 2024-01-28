7784607d971ce4fb0baabaa2229609cc9c889c62
aml494 <amalossman494@gmail.com>
2024-01-28 20:24:41 +0200

    1

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,9 +1,3 @@
 def add(a, b):
-    return a + b
-add = __import__('0-sum').add
-
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
-
-
+    """Return the addition of a and b."""
+    return (a + b)
